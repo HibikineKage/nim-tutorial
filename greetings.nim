01c81cc705d6790e60d93f4f0d23461c3b140b4e
Hibikine Kage <goodsmile314159@gmail.com>
2018-07-11 14:52:59 +0900

    initial commit

---
+++ greetings.nim
@@ -0,0 +1,4 @@
+# This is a comment
+echo "What's your name? "
+let name: string = readline(stdin)
+echo "Hi, ", name, "!"
---
+++ tutorial.nim
@@ -0,0 +1,16 @@
+let rawString = r"raw string\\ \ \"
+
+# A comment.
+let myVariable: int = 5 ## a document commentttt
+#[
+  Multiline Commenting
+]#
+
+var
+  x, y: int
+  a,b,c: string
+
+a = "abc"
+a = "xyz"
+
+const hoge = "fuga"
